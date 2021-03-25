0c39eea89fe91492b72597eff3705bad73d51d05
abarv <abarv@DESKTOP-P29N5AN>
2021-03-26 02:02:29 +0530

    Final commit

---
+++ FirstWebApp/src/main/java/com/example/demo/homecontroller.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class homecontroller {
+    @RequestMapping("Home")
+   // @ResponseBody
+	public String home()
+	{
+		System.out.println("HI");
+		return "Home.jsp"; 
+		
+		
+		
+	}
+
+}
